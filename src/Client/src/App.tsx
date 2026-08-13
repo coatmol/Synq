e337892299c6c568ddbc6ca759b64a30126c353d
Coatmol <omarelgoh08@gmail.com>
2026-08-14 02:53:36 +0300

    feat: add update check on startup with notification for available updates

--- src/Client/src/App.tsx
+++ src/Client/src/App.tsx
@@ -3,11 +3,29 @@ import {AppLayout} from "./components/AppLayout";
 import {EditorWorkspace} from "./components/EditorWorkspace";
 import {WelcomeScreen} from "./components/WelcomeScreen";
 import {SettingsModal} from "./components/SettingsModal";
+import {api} from "./api";
+import {toast} from "sonner";
 
 function App() {
   const [view, setView] = useState<"welcome" | "editor">("welcome");
 
   useEffect(() => {
+    // Check for updates on startup
+    api.checkForUpdates().then(updateInfo => {
+      if (updateInfo?.updateAvailable) {
+        toast.info("Update Available", {
+          description: `Synq version ${updateInfo.latest} is available!`,
+          duration: 10000,
+          action: {
+            label: "Open Settings",
+            onClick: () => window.dispatchEvent(new Event('open-settings'))
+          }
+        });
+      }
+    }).catch(() => {
+      // Silently ignore update check errors on startup
+    });
+
     if (typeof window !== 'undefined' && (window as any).external && (window as any).external.receiveMessage) {
       (window as any).external.receiveMessage((msg: string) => {
         if (msg === 'folderOpened') {
